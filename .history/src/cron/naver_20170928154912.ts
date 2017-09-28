23d85f3a3f55962488e56181b5f6ac79105685d8
Ryan Kim Ramos <kaimyo24@gmail.com>
2017-09-28 19:13:03 +0800

    work on history

--- .history/src/cron/naver_20170928154912.ts
+++
@@ -1,9 +0,0 @@
-const spawn = require('child_process').spawn;
-var CronJob = require('cron').CronJob;
-new CronJob('00 * * * * *', function() {
-  console.log('Cron runs every 5 minutes.');
-  spawn('node', ['dist/task/naver.kin.desktop.js', '--keyword=화상영어']);
-  spawn('node', ['dist/task/naver.kin.desktop.js', '--keyword=어린이영어']);
-  spawn('node', ['dist/task/naver.kin.mobile.js', '--keyword=화상영어']);
-  spawn('node', ['dist/task/naver.kin.mobile.js', '--keyword=어린이영어']);
-}, null, true, 'Asia/Seoul');
